b5982f6e960223007797f77fe9634cf3fd690b8e
geots <77642975+Geots@users.noreply.github.com>
2022-08-17 15:46:28 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import subprocess
+
+def clear():
+	subprocess.run(["clear"])
+	print('Story telling game by lavadamage')
+	print('')
+	print('')
+
+clear()
+
+print('Start your story:')
+print('')
+s = input('')
